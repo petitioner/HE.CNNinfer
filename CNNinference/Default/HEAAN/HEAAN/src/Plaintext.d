4ffd6593150a382b5ebe4be984b37cf2256b56dd
John C. Smith <51513317+petitioner@users.noreply.github.com>
2022-04-09 11:55:42 +0800

    Delete Plaintext.d

--- CNNinference/Default/HEAAN/HEAAN/src/Plaintext.d
+++
@@ -1,4 +0,0 @@
-HEAAN/HEAAN/src/Plaintext.o: ../HEAAN/HEAAN/src/Plaintext.cpp \
- ../HEAAN/HEAAN/src/Plaintext.h ../HEAAN/HEAAN/src/Params.h
-../HEAAN/HEAAN/src/Plaintext.h:
-../HEAAN/HEAAN/src/Params.h:
