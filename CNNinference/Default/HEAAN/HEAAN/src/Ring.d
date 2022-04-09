b362eb48f1983e772f1821c655dc030599c7582d
John C. Smith <51513317+petitioner@users.noreply.github.com>
2022-04-09 11:55:34 +0800

    Delete Ring.d

--- CNNinference/Default/HEAAN/HEAAN/src/Ring.d
+++
@@ -1,9 +0,0 @@
-HEAAN/HEAAN/src/Ring.o: ../HEAAN/HEAAN/src/Ring.cpp \
- ../HEAAN/HEAAN/src/Ring.h ../HEAAN/HEAAN/src/BootContext.h \
- ../HEAAN/HEAAN/src/RingMultiplier.h ../HEAAN/HEAAN/src/Params.h \
- ../HEAAN/HEAAN/src/EvaluatorUtils.h
-../HEAAN/HEAAN/src/Ring.h:
-../HEAAN/HEAAN/src/BootContext.h:
-../HEAAN/HEAAN/src/RingMultiplier.h:
-../HEAAN/HEAAN/src/Params.h:
-../HEAAN/HEAAN/src/EvaluatorUtils.h:
