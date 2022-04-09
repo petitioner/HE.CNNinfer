5e11f52d6e3b6d596eb0582c5ef8000ce1ebb9e6
John C. Smith <51513317+petitioner@users.noreply.github.com>
2022-04-09 11:55:25 +0800

    Delete RingMultiplier.d

--- CNNinference/Default/HEAAN/HEAAN/src/RingMultiplier.d
+++
@@ -1,4 +0,0 @@
-HEAAN/HEAAN/src/RingMultiplier.o: ../HEAAN/HEAAN/src/RingMultiplier.cpp \
- ../HEAAN/HEAAN/src/RingMultiplier.h ../HEAAN/HEAAN/src/Params.h
-../HEAAN/HEAAN/src/RingMultiplier.h:
-../HEAAN/HEAAN/src/Params.h:
