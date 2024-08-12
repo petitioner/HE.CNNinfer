89c832e63d0c6ce8ab0ee5b7ed8d9625f5f120ad
John C. Smith <51513317+petitioner@users.noreply.github.com>
2024-08-12 21:05:37 +0800

    GDSSFESFESF

--- CNNinference/Default/HEAAN/HEAAN/run/subdir.mk
+++ CNNinference/Default/HEAAN/HEAAN/run/subdir.mk
@@ -2,23 +2,28 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
+# Define the project root path
+PROJECT_ROOT := $(abspath $(dir $(firstword $(MAKEFILE_LIST)))/../../../..)
+
+# Directories
+RUN_SRC_DIR := $(PROJECT_ROOT)/HEAAN/HEAAN/run
+INCLUDE_DIR := $(PROJECT_ROOT)/lib/include
+HEAAN_INCLUDE_DIR := $(PROJECT_ROOT)/HEAAN/HEAAN/src
+
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../HEAAN/HEAAN/run/test.cpp 
+$(RUN_SRC_DIR)/test.cpp 
 
 OBJS += \
-./HEAAN/HEAAN/run/test.o 
+$(RUN_SRC_DIR)/test.o 
 
 CPP_DEPS += \
-./HEAAN/HEAAN/run/test.d 
-
+$(RUN_SRC_DIR)/test.d 
 
 # Each subdirectory must supply rules for building sources it contributes
-HEAAN/HEAAN/run/%.o: ../HEAAN/HEAAN/run/%.cpp
+%.o: $(RUN_SRC_DIR)/%.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/home/tree/Downloads/IDASH2017-master/IDASH2017/HEAAN/HEAAN/src" -I"/home/tree/Downloads/IDASH2017-master/IDASH2017/lib/include" -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Invoking: GCC C++ Compiler 4 Testing'
+	g++ -I$(HEAAN_INCLUDE_DIR) -I$(INCLUDE_DIR) -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
-
-
