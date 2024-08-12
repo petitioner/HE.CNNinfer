de6b648e9b2d954aa33b74ed4444798ac505b4c0
John C. Smith <51513317+petitioner@users.noreply.github.com>
2024-08-12 21:58:06 +0800

    FSFEFSEF

--- CNNinference/Default/HEAAN/HEAAN/run/subdir.mk
+++ CNNinference/Default/HEAAN/HEAAN/run/subdir.mk
@@ -2,28 +2,22 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-# Define the project root path
-PROJECT_ROOT := $(abspath $(dir $(firstword $(MAKEFILE_LIST)))/../../../..)
-
-# Directories
-RUN_SRC_DIR := $(PROJECT_ROOT)/HEAAN/HEAAN/run
-INCLUDE_DIR := $(PROJECT_ROOT)/lib/include
-HEAAN_INCLUDE_DIR := $(PROJECT_ROOT)/HEAAN/HEAAN/src
-
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-$(RUN_SRC_DIR)/test.cpp 
+../HEAAN/HEAAN/run/test.cpp 
 
 OBJS += \
-$(RUN_SRC_DIR)/test.o 
+./HEAAN/HEAAN/run/test.o 
 
 CPP_DEPS += \
-$(RUN_SRC_DIR)/test.d 
+./HEAAN/HEAAN/run/test.d 
+
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: $(RUN_SRC_DIR)/%.cpp
+HEAAN/HEAAN/run/%.o: ../HEAAN/HEAAN/run/%.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C++ Compiler 4 Testing'
-	g++ -I$(HEAAN_INCLUDE_DIR) -I$(INCLUDE_DIR) -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"/root/HE.CNNinfer/CNNinference/HEAAN/HEAAN/src" -I"/root/HE.CNNinfer/CNNinference/lib/include" -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
+
