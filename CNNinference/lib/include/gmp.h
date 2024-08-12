d492e05eaf80bbd96aeb8437869703cf98591809
John C. Smith <51513317+petitioner@users.noreply.github.com>
2024-08-12 15:32:56 +0800

    Add files via upload

--- CNNinference/lib/include/gmp.h
+++ CNNinference/lib/include/gmp.h
@@ -2317,7 +2317,7 @@ enum
 
 /* Define CC and CFLAGS which were used to build this version of GMP */
 #define __GMP_CC "gcc"
-#define __GMP_CFLAGS "-O2 -pedantic -fomit-frame-pointer -m64 -mtune=broadwell -march=broadwell"
+#define __GMP_CFLAGS "-O2 -pedantic -fomit-frame-pointer -m64 -mtune=k8 -march=k8"
 
 /* Major version number is the value of __GNU_MP__ too, above. */
 #define __GNU_MP_VERSION            6
