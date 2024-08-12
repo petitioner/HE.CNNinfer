280f75451e7f65e2c7d7dea3c754074d004bbba1
John C. Smith <51513317+petitioner@users.noreply.github.com>
2024-08-12 20:14:11 +0800

    修改後的makefile 支持相對路徑 （之前的是採用SNU的makefile 支持絕對路徑的編譯命令）
    修改後的makefile 支持相對路徑 （之前的是採用SNU的makefile 支持絕對路徑的編譯命令）

--- CNNinference/Default/src/subdir.mk
+++ CNNinference/Default/src/subdir.mk
@@ -21,13 +21,10 @@ CPP_DEPS += \
 ./src/Methods.d \
 ./src/Tools.d 
 
-
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/home/john/eclipse-workspace/CNNinference/HEAAN/HEAAN/src" -I"/home/john/eclipse-workspace/CNNinference/lib/include" -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I$(INCLUDE_DIR) -I$(HEAAN_INCLUDE_DIR) -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
-
-
