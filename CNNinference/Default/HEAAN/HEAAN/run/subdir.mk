eac80e4455a22086f05b03250029a061943df5f1
John C. Smith <51513317+petitioner@users.noreply.github.com>
2024-08-12 23:08:36 +0800

    Update subdir.mk

--- CNNinference/Default/HEAAN/HEAAN/run/subdir.mk
+++ CNNinference/Default/HEAAN/HEAAN/run/subdir.mk
@@ -16,7 +16,7 @@ CPP_DEPS += \
 # Each subdirectory must supply rules for building sources it contributes
 HEAAN/HEAAN/run/%.o: ../HEAAN/HEAAN/run/%.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C++ Compiler'
+	@echo 'Invoking: GCC C++ Compiler [CNNinference/Default/HEAAN/HEAAN/run/subdir.mk]'
 	g++ -I"/root/HE.CNNinfer/CNNinference/HEAAN/HEAAN/src" -I"/root/HE.CNNinfer/CNNinference/lib/include" -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
